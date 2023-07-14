3f33280bded99955f106e17d9bc734de97793cc0
Trip Tate <trip.tate@gmail.com>
2023-07-14 13:35:55 -0600

    refactor according to lesson 2.12

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,14 +26,17 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display FruityVice API response
 streamlit.header("Fruityvice Fruit Advice!")
+
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  return pandas.json_normalize(fruityvice_response.json())
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(get_fruityvice_data(fruit_choice))
 except URLError as e:
   streamlit.error()
 
